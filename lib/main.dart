7799213266d5a266b8fb844481fc65a53f375ebf
Lins <73877243+Lins-bot@users.noreply.github.com>
2025-11-26 16:09:52 +0100

    Register fr locale

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() async {
   
   runApp(
     EasyLocalization(
-      supportedLocales: const [Locale('en', 'US'), Locale('es')],
+      supportedLocales: const [Locale('en', 'US'), Locale('es'), Locale('fr')],
       path: 'assets/i18n',
       fallbackLocale: const Locale('en', 'US'),
       child: const MyApp(),
